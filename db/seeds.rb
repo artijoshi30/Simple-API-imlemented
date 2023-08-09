a00017bfbb821e96506bdaea47f70d7065a39f6d
Arti <arti@gmail.com>
2023-08-09 18:25:58 +0530

    Imlemented simple api

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+ Product.create([
+ 	{ name: "Star Wars", brand: "Lord of the Rings", price: "200$"},
+ 	{ name: "A", brand: "Lord of the Rings", price: "200$"},
+ 	{ name: "B", brand: "Lord of the Rings", price: "600$"},
+ 	{ name: "C", brand: "Lord of the Rings", price: "200$"},
+ 	{ name: "D", brand: "Lord of the Rings", price: "500$"},
+ 	{ name: "E", brand: "Lord of the Rings", price: "200$"},
+ 	{ name: "F", brand: "Lord of the Rings", price: "100$"},
+])
